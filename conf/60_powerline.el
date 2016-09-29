d59593e70f118652864c69e379e3c9bd6333f376
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2016-09-29 23:55:03 +0900

    powerlineの見た目が良くなる設定

--- conf/60_powerline.el
+++ conf/60_powerline.el
@@ -2,6 +2,10 @@
 
 (use-package powerline
   :config
+  (if (mac?)
+      ;; これが無いと powerline のみためが悪い
+      ;; cf. https://github.com/milkypostman/powerline/issues/54#issuecomment-65078550
+      (setq ns-use-srgb-colorspace nil))
   (defun my-powerline ()
     (set-face-attribute 'mode-line nil
                         :foreground "white"
