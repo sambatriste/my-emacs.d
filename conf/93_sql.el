f5bbb5764dc91a80750c19e1976dfe46250ef45c
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2016-10-09 13:34:13 +0900

    SQL文の整形をJavaで行うようにする。

--- conf/93_sql.el
+++ conf/93_sql.el
@@ -1,29 +1,33 @@
 ;;; -*- coding: utf-8-unix -*-
 
-
-;;http://haraita9283.blog98.fc2.com/blog-entry-397.html
-;;SQLを整形する
-(defvar anbt-sql-formatter:formatter-path
-  "~/.emacs.d/lib/anbt-sql-formatter-master/bin/anbt-sql-formatter")
-(defun anbt-sql-formatter:format ()
-  "Format SQL on specified region."
+;;; SQL文の整形をする設定
+(defvar sql-format-external-command
+  (concat "java -jar " (expand-file-name "~/.emacs.d/lib/sql-formatter-all-1.0.0.jar"))
+  "SQLをフォーマットするための外部コマンド")
+(defun my-format-sql ()
+  "バッファまたはリージョン内のSQL文を整形する。"
   (interactive)
-  (destructuring-bind (begin end)
-      (if mark-active
-          (list (region-beginning) (region-end))
-        (list (point-min) (point-max)))
-    (format-sql begin end)))
-
-(defun format-sql (begin end)
-  (save-excursion
-    (shell-command-on-region
-     begin
-     end
-     (format "ruby %s" (expand-file-name anbt-sql-formatter:formatter-path)) nil t)))
+  (let (begin end)
+    (cond (mark-active
+           (setq begin (region-beginning))
+           (setq end (region-end)))
+          (t
+           (setq begin (point-min))
+           (setq end (point-max))))
+    (save-excursion
+      (shell-command-on-region
+       begin
+       end
+       sql-format-external-command
+       nil 
+       t ; replace buffer
+       ))))
 
 (add-hook 'sql-mode-hook
           (lambda ()
-            (define-key sql-mode-map (kbd "C-S-f") 'anbt-sql-formatter:format)))
+            (define-key sql-mode-map (kbd "C-S-f") 'my-format-sql)))
+
+
 
 ;; sql-modeの文字化け対策
 (add-hook 'sql-interactive-mode-hook
