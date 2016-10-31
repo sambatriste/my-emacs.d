436e606cfb2020e8241bfb50da7b426c49a5471b
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2016-10-31 11:43:08 +0900

    serverがうまく立ち上がらないことがある問題に対処.

--- conf/38_server.el
+++ conf/38_server.el
@@ -1,4 +1,6 @@
 
+
+(message "try to start server...")
 (when (require 'server nil t)
   ;;http://stackoverflow.com/questions/885793/emacs-error-when-calling-server-start
   ;; Suppress error "directory ~/.emacs.d/server is unsafe" on windows.
@@ -6,5 +8,12 @@
       (defun server-ensure-safe-dir (dir) "Noop" t))
   (global-set-key (kbd "C-x C-c") 'server-edit)
   (defalias 'exit 'save-buffers-kill-emacs)
-  (unless (server-running-p)
-    (if window-system (server-start))))
+  ;; try to start server if neccesary.
+  (cond ((and window-system (not (eq (server-running-p) t)))
+         (server-start)
+         ;; check
+         (cond ((eq (server-running-p) t)
+                (message (concat "server started.")))
+               (t
+                (error "failed to start server."))))
+        (t (message "no need to start server..."))))
