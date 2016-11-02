471576ce709624f6928a0695703c83b06469b215
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2016-11-02 13:56:18 +0900

    リファクタリング.

--- conf/57_markdown.el
+++ conf/57_markdown.el
@@ -6,10 +6,13 @@
   (save-excursion
     (goto-char (point-min))
     ;; |----+-----+----|の行を見つけたら|----|-----|----|に置き換える
-    (while (re-search-forward "^\s*|\\(-+\\(\\+\\)\\)+-+|\s*$" nil t)
-      (let ((end (point))
-            (begin (point-at-bol)))
-        (replace-string "+" "|" nil begin end)))))
+    (while (re-search-forward (rx bol (* (syntax whitespace)) "|" ;; 行頭 "|"
+                                  (+ (+ "-") (group "+"))         ;; "---+ の連続 ("+" だけキャプチャ)
+                                  (+ "-") "|"                     ;; 最後の列 "|"
+                                  (* "\s") eol)                   ;; 行末
+                              nil t)
+        (replace-match (replace-regexp-in-string (rx "+") "|" (match-string-no-properties 0))))))
+
 (defun my-markdown-hook ()
   (orgtbl-mode t)
   (add-hook 'before-save-hook
