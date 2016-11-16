fa16df05c77735c18e30f7ffb45d1e0f214b12be
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2016-11-16 10:10:35 +0900

    package取得をhttpsでなくhttpにする.

--- conf/20_package.el
+++ conf/20_package.el
@@ -2,9 +2,9 @@
 
 (require 'package)
 (setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
+      '(("gnu" . "http://elpa.gnu.org/packages/")
         ("melpa-stable" . "http://stable.melpa.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
         ;("marmalade" . "https://marmalade-repo.org/packages/")
         ))
 
