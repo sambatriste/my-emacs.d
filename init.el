7a978e977d687300dcb7655ad69cca7ca269c4d7
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2016-11-17 13:20:27 +0900

    package-initializeが勝手に挿入される問題に対処.

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
 (let ((my-emacsd "~/.emacs.d/"))
    (add-to-list 'load-path (expand-file-name  "elisp" my-emacsd))
    (require 'init-loader)
