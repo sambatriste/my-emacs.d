d182559fd4bdf785524899f45006f17f5c3225e4
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2016-12-07 13:01:11 +0900

    recentfの設定を微調整.

--- conf/30_recentf.el
+++ conf/30_recentf.el
@@ -3,4 +3,8 @@
   :config
   (setq recentf-max-menu-items 300)
   (setq recentf-max-saved-items 300)
+  ;; 除外設定
+  (setq recentf-exclude '("/recentf" "COMMIT_EDITMSG"))
+  ;; 30秒に一度自動保存する
+  (setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list))
   (recentf-mode t))
