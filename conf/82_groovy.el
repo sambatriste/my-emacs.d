958f5aa2c2fc35bea61830e1ca711783719c1afd
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2016-12-27 15:31:56 +0900

    lambdaをやめて明示的に関数を定義する.

--- conf/82_groovy.el
+++ conf/82_groovy.el
@@ -1,8 +1,11 @@
 ;;; -*- coding: utf-8-unix -*-
 
+
+(defun my-enable-groovy-electric-mode ()
+  (require 'groovy-electric)
+  (groovy-electric-mode))
+
 (use-package groovy-mode
   :config
   (add-hook 'groovy-mode-hook
-            '(lambda ()
-               (require 'groovy-electric)
-               (groovy-electric-mode))))
+            'my-enable-groovy-electric-mode))
