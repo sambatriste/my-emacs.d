a30b7da533b6f8e335a9d93f2ae3f5acf9f96941
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2017-05-03 18:27:43 +0900

    company-dabbrevで日本語を補完しない.

--- conf/35_company.el
+++ conf/35_company.el
@@ -4,6 +4,20 @@
   :diminish company-mode
   :config
   (global-company-mode)
+
+  ;; company-dabbrevで日本語を補完しない
+  ;; http://qiita.com/wktkshn/items/3ac46671d1c242a59f7e
+  (defun edit-category-table-for-company-dabbrev (&optional table)
+    (define-category ?s "word constituents for company-dabbrev" table)
+    (let ((i 0))
+      (while (< i 128)
+        (if (equal ?w (char-syntax i))
+            (modify-category-entry i ?s table)
+          (modify-category-entry i ?s table t))
+        (setq i (1+ i)))))
+  (edit-category-table-for-company-dabbrev)
+  (setq company-dabbrev-char-regexp "\\cs")
+  
   (setq company-idle-delay 0.3
         company-minimum-prefix-length 4
         company-selection-wrap-around t
@@ -20,4 +34,5 @@
              ("C-n" . company-select-next)
              ("C-p" . company-select-previous))
   (global-set-key (kbd "<tab>") 'indent-according-to-mode))
+
 
