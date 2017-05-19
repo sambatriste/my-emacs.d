b70e031487e5b0d61f1649f568e475c934239502
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2017-05-19 09:54:14 +0900

    revealの設定を変更.

--- conf/59_presentation.el
+++ conf/59_presentation.el
@@ -1,12 +1,10 @@
 
 
-(use-package org-present)
+;(use-package org-present)
 
 (use-package ox-reveal
   :config
   (setq org-reveal-root (concat "file:///" (expand-file-name "~/.emacs.d/lib/reveal.js/"))
-        org-reveal-center nil)
-  
-  )
+        org-reveal-center nil))
 
 (use-package htmlize)
