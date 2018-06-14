2bf3a27d77243c61671e4e7596bb46eb595f4571
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2018-06-14 21:37:57 +0900

    mepla-stableだけでなくmeplaも使うようにする.

--- conf/20_package.el
+++ conf/20_package.el
@@ -1,14 +1,18 @@
 ;;; -*- coding: utf-8-unix -*-
 
 (require 'package)
-(setq package-archives
-      '(("melpa-stable" . "http://stable.melpa.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")
-        ;("marmalade" . "https://marmalade-repo.org/packages/")
-        ))
 
 
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+ ; (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  
+)
 (package-initialize)
+
 (unless package-archive-contents (package-refresh-contents))
 
 (when (not (require 'use-package nil t))
@@ -17,6 +21,6 @@
 ;;; enable use-package
 (require 'use-package)
 (setq use-package-always-ensure t)
-(setq use-package-always-pin "melpa-stable")
+;(setq use-package-always-pin "melpa-stable")
 (setq use-package-verbose t)
 
