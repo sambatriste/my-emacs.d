fc0be8dd6b7c6bd591efade85d82062ee28bfa36
Tsuyoshi Kawasaki <kawasaki.tsuyoshi@tis.co.jp>
2019-10-03 15:07:34 +0900

    recentfの除外設定に一時ディレクトリを追加.

--- conf/30_recentf.el
+++ conf/30_recentf.el
@@ -4,7 +4,7 @@
   (setq recentf-max-menu-items 300)
   (setq recentf-max-saved-items 300)
   ;; 除外設定
-  (setq recentf-exclude '("/recentf" "COMMIT_EDITMSG"))
+  (setq recentf-exclude '("/recentf" "COMMIT_EDITMSG" "AppData/Local/Temp/"))
   ;; 30秒に一度自動保存する
   (setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list))
   (recentf-mode t))
