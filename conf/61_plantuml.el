16f37a89a825b4998c135d9403acf1ca873a54d1
Tsuyoshi Kawasaki <kawasaki.tsuyoshi@tis.co.jp>
2019-10-03 15:09:33 +0900

    plantumlのオプションを追加.

--- conf/61_plantuml.el
+++ conf/61_plantuml.el
@@ -4,4 +4,6 @@
   (("\\.puml\\'" . plantuml-mode))
   
   :config
-  (setq plantuml-jar-path (expand-file-name "~/.emacs.d/lib/plantuml.jar")))
+  (setq plantuml-jar-path (expand-file-name "~/.emacs.d/lib/plantuml.jar"))
+  (setq plantuml-output-type "png")
+  (setq plantuml-options "-charset UTF-8"))
