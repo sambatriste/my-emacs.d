ed0dfb0c66d02ca868f2c200bbb22b2d7bedfece
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2021-03-14 10:31:24 +0900

    SQLフォーマットを改善.

--- conf/93_sql.el
+++ conf/93_sql.el
@@ -2,7 +2,7 @@
 
 ;;; SQL文の整形をする設定
 (setq sql-format-external-command
-  (concat "java -jar " (expand-file-name "~/.emacs.d/lib/sql-formatter-1.0.0-jar-with-dependencies.jar")))
+  (concat "java -jar " (expand-file-name "~/.emacs.d/lib/sql-formatter-1.0.1-jar-with-dependencies.jar")))
 ;; SQL文をフォーマットする関数
 (defun my-format-sql ()
   "バッファまたはリージョン内のSQL文を整形する。"
@@ -33,3 +33,7 @@
 (add-hook 'sql-interactive-mode-hook
           (function (lambda ()
                       (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
+
+(use-package sql-indent
+  :init
+  (add-hook 'sql-mode-hook 'sqlind-minor-mode))
